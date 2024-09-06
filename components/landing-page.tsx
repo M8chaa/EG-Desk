73ff359b1eceeadde9f102ba8d1660b8145d8d7e
MinsooCha <m8chaa@gmail.com>
2024-09-06 11:10:40 +0900

    firebase config to next_public

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -73,7 +73,7 @@ const LandingPage = () => {
             Contact
           </Link>
         </nav>
-        <Login /> {/* Use the Login component here */}
+        {/* <Login /> Use the Login component here */}
       </header>
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
